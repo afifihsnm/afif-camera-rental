7959057f8736a8de286885e39b2a156c7ade8ad5
afifihsnm <afifihsanke2@gmail.com>
2025-04-25 00:12:52 +0700

    fix: update logo and web title

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title inertia>Bayu Rental Kamera</title>
+    <title inertia>Afif Camera Rental</title>
 
     <!-- Scripts -->
     @routes
